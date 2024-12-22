c9ca860f2a9fb42e15d24fa8b348ccd7d275bd86
Xalil404 <kareemxalil@gmail.com>
2024-12-22 13:05:37 +0000

    Added landing page and navigation

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+// components/Header.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = ({ isAuthenticated }) => {
+  return (
+    <div className="navcolor">
+      <nav className="navbar navbar-expand-lg navbar-light py-3 sticky-top">
+        <div className="container-fluid">
+          {/* Logo (Top Left) */}
+          <Link className="navbar-brand ms-5 order-2 order-lg-1" href="/">
+            <Image
+              src="https://res.cloudinary.com/dnbbm9vzi/image/upload/v1734872641/Screenshot_2024-12-22_at_1.03.09_PM-removebg-preview_wg8rtk.png"
+              alt="Logo"
+              className="logo-image"
+              width={200}
+              height={50}
+            />
+          </Link>
+
+          {/* Navbar Links (Top Left) */}
+          <div className="collapse navbar-collapse order-3 order-lg-2" id="navbarText">
+            <ul className="navbar-nav mb-2 mb-lg-0 me-auto">
+              {isAuthenticated && (
+                <>
+                  <li className="nav-item mx-2">
+                    <Link className="nav-link-nav active text-dark" href="/dashboard">
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <Link className="nav-link-nav active text-dark" href="/logout">
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+
+          {/* Sign In and Sign Up Buttons (Top Right) */}
+          <div className="d-flex order-2 order-lg-3 align-items-center gap-3">
+            {!isAuthenticated && (
+              <>
+                <Link className="nav-link-a d-none d-lg-block text-dark" href="/login">
+                  Sign in
+                </Link>
+
+                <Link
+                  className="nav-link btn btn-sm rounded-pill px-4"
+                  style={{
+                    backgroundColor: "#E8BF73",
+                    color: "black",
+                    padding: "10px 20px",
+                    marginRight: "10px",
+                    marginLeft: "5px",
+                  }}
+                  href="/register"
+                >
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
